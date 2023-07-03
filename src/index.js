47e9ac53ba1c856a622221df8d86595bfe508fd9
viniciusMartinsDev <mpviniciusmp@gmail.com>
2023-07-03 00:01:07 -0300

    adiciona o `cors`

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const personRoute = require("./routes/PersonRoutes");
 
+app.use(cors());
 app.use(express.json());
 app.use(personRoute);
 
